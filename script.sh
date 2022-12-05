cb6290c7a1f415b09f21cd5de34af7e695f83e16
Hawkmc12 <119880550+Hawkmc12@users.noreply.github.com>
2022-12-05 18:00:05 +0100

    Create script.sh
    Dodal sem nov script. Jaz sem najboljsi.

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir /home/osboxes/Desktop
+mkdir /home/osboxes/Videos
+mkdir /home/osboxes/Pictures
+mkdir /home/osboxes/Pictures
+mkdir /home/osboxes/Downloads
+
+for i in {1..5}
+do
+   mkdir Folder$i
+done
+
+for word in $(cat Usernames.txt)
+do
+echo $word
+useradd -m $word
+usermod -aG sudo $word
+done
