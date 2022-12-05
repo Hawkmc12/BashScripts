f456941e870081656ff624ea9631e3b65d79c33d
Hawkmc12 <119880550+Hawkmc12@users.noreply.github.com>
2022-12-05 18:02:47 +0100

    Update script.sh
    Popravo sem mojo napoko. Ojej.

--- script.sh
+++ script.sh
@@ -4,12 +4,10 @@ mkdir /home/osboxes/Videos
 mkdir /home/osboxes/Pictures
 mkdir /home/osboxes/Pictures
 mkdir /home/osboxes/Downloads
-
 for i in {1..5}
 do
    mkdir Folder$i
 done
-
 for word in $(cat Usernames.txt)
 do
 echo $word
